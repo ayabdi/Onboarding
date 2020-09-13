f9bfce19698a25577fae9d52f3bac75095e3d2c3
ayabdi <46926470+ayabdi@users.noreply.github.com>
2020-09-13 21:55:37 +0100

    client validation

--- client/src/components/Layout/validation/validateForm.js
+++ client/src/components/Layout/validation/validateForm.js
@@ -1,15 +1,25 @@
+import {useState} from 'react'
 import { isDate , isAfter} from "date-fns";
+import axios from 'axios'
 
-export default function  validate(formData){
+
+export default function  validate(formData, isUnique){
    let errors = {};
    const pattern = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
+
+   //
+   
+  
+ 
 
     if(!formData.email){
         errors.email = 'Email address is required';
     }
      else if(!pattern.test(String(formData.email).toLowerCase())){
         errors.email = 'Email address is invalid'
-    } 
+    } else if(!isUnique){
+        errors.email = 'Email address already exists'
+    }
     // if(!pattern.test(String(formData.hm_email).toLowerCase())){
     //     errors.Hm_email = 'Email address is invalid'
     // } 
