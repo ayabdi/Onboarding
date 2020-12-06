8f18428e04493dce253e1fc18eea714ee3d6af3a
ayabdi <46926470+ayabdi@users.noreply.github.com>
2020-12-06 15:06:41 +0000

    switching back to nodemailer

--- routes/mailer.js
+++ routes/mailer.js
@@ -1,42 +1,44 @@
 const cron = require("node-cron");
+const nodemailer = require("nodemailer");
 const express = require("express");
 const addDays = require("date-fns/addDays");
 const parseISO = require("date-fns/parseISO");
 const datefns = require("date-fns");
-const AWS = require("aws-sdk");
 const moment = require('moment')
 
 require("dotenv").config();
 
 const sendmailRouter = express.Router();
-//const bodyParser = require("body-parser");
 
-const SES_CONFIG = {
-  accessKeyId: "AKIAQ67PSZM2FI64BLE4",
-  secretAccessKey: "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",
-  region: "us-east-2",
+const transport = {
+  //all of the configuration for making a site send an email.
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
 };
 
-const AWS_SES = new AWS.SES(SES_CONFIG);
-
-var from = "harmonize.onboarding@gmail.com";
+const transporter = nodemailer.createTransport(transport);
+transporter.verify((error, success) => {
+  if (error) {
+    //if error happened code ends here
+    console.error(error);
+  } else {
+    //this means success
+    console.log("users ready to mail");
+  }
+});
 
 sendmailRouter.post("/email", (req, res, next) => {
-  var ses_mail = "From: 'Harmonize Test Email' <" + from + ">\n";
-  ses_mail = ses_mail + `To:  ${req.body.to} \n`;
-  ses_mail = ses_mail + `Subject: ${req.body.subject}\n`;
-  ses_mail = ses_mail + "MIME-Version: 1.0\n";
-  ses_mail =
-    ses_mail + 'Content-Type: multipart/mixed; boundary="NextPart"\n\n';
-  ses_mail = ses_mail + "--NextPart\n";
-  ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
-  ses_mail = ses_mail + `<p>${req.body.message} </p>\n\n`;
-  ses_mail = ses_mail + "--NextPart\n";
-
+  //make mailable object
   const mail = {
-    Source: "harmonize.onboarding@gmail.com",
-    Destinations: [req.body.to],
-    RawMessage: { Data: new Buffer.from(ses_mail) },
+    from: process.env.EMAIL,
+    to: req.body.to,
+    subject: req.body.subject,
+    text: req.body.message,
   };
 
   //calculate email date and set month and day
@@ -45,28 +47,31 @@ sendmailRouter.post("/email", (req, res, next) => {
   var month = datefns.getMonth(emaildate) + 1;
   var minute = datefns.getMinutes(new Date()) ;
   var hour = datefns.getHours(new Date());
-  var second = datefns.getSeconds(new Date()) + 2;
+  var second = datefns.getSeconds(new Date()) + 2
 
   //scheduler
   cron.schedule(
     `${second} ${minute} ${hour} ${day} ${month} *`,
     () => {
-      AWS_SES.sendRawEmail(mail, (err, data) => {
+      console.log("email sent");
+      console.log(day);
+      transporter.sendMail(mail, (err, data) => {
+        // error handling goes here.
         if (err) {
           res.json({
             status: "fail",
           });
         } else {
-          console.log("emails sent");
-          // res.json({
-          //   status: "success",
-          // });
+          res.json({
+            status: "success",
+          });
         }
       });
     },
     {
       scheduled: true,
       timezone: "Europe/London",
+
     }
   );
 });
@@ -75,70 +80,60 @@ sendmailRouter.post("/email", (req, res, next) => {
 
 sendmailRouter.post("/task", (req, res, next) => {
   //make mailable object
-  var ses_mail = "From: 'Harmonize Test Tasks' <" + from + ">\n";
-  ses_mail = ses_mail + "To: " + req.body.to_email + "\n";
-  ses_mail = ses_mail + `Subject: ${req.body.task}\n`;
-  ses_mail = ses_mail + "MIME-Version: 1.0\n";
-  ses_mail =
-    ses_mail + 'Content-Type: multipart/mixed; boundary="NextPart"\n\n';
-  ses_mail = ses_mail + "--NextPart\n";
-  ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
-  ses_mail =
-    ses_mail +
-    `<p>${req.body.task}
-          <br></br>
-          Due Date: ${moment(datefns.parseISO(req.body.hire.startDate)).toDate().toLocaleString("default", {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-           })}
-          <br></br>
-          ${req.body.note}
-           </p>,\n\n`;
-  // ses_mail = ses_mail + "--NextPart\n";
-
   const mail = {
-    Source: "harmonize.onboarding@gmail.com",
-    Destinations: [req.body.to_email],
-    RawMessage: { Data: new Buffer.from(ses_mail) },
+    from: process.env.EMAIL,
+    to: req.body.to_email,
+    subject: "Automated Email reminder from " + req.body.from,
+    //text: req.body.message,
+    html: `<p>${req.body.task}
+             <br></br>
+             Due Date: ${moment(datefns.parseISO(req.body.hire.startDate)).toDate().toLocaleString("default", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+             })}
+             <br></br>
+             ${req.body.note}
+          </p>`,
   };
 
   //calculate email date and set month and day
   var reminderArr = req.body.reminder.split(",");
-  var minute = datefns.getMinutes(new Date())  ;
-  var hour = datefns.getHours(new Date());
-  var second = datefns.getSeconds(new Date()) + 2;
+  var minute = datefns.getMinutes(new Date()) ;
+  var hour = datefns.getHours(new Date()) ;
+  var second = datefns.getSeconds(new Date()) + 2
 
   //scheduler
-    reminderArr.map((reminder, i) => {
-      var emaildate = addDays(
-        parseISO(req.body.hire.startDate),
-        -reminder - req.body.due_date
-      );
-      var day = datefns.getDate(emaildate);
-      var month = datefns.getMonth(emaildate) + 1;
-      cron.schedule(
-        `${second} ${minute} ${hour} ${day} ${month} *`,
-        () => {
-          AWS_SES.sendRawEmail(mail, (err, data) => {
+  reminderArr.map((reminder) => {
+    var emaildate = addDays(
+      parseISO(req.body.hire.startDate),
+      -reminder - req.body.due_date
+    );
+    var day = datefns.getDate(emaildate);
+    var month = datefns.getMonth(emaildate) + 1;
+    cron.schedule(
+      `${second} ${minute} ${hour} ${day} ${month} *`,
+      () => {
+        console.log("task sent");
+        console.log(req.body.hire.startDate);
+        transporter.sendMail(mail, (err, data) => {
+          // error handling goes here.
           if (err) {
             res.json({
               status: "fail",
             });
           } else {
-            console.log("task sent");
-            // res.json({
-            //   status: "success",
-            // });
+            res.json({
+              status: "success",
+            });
           }
-        })
-        },
-        {
-          scheduled: true,
-          timezone: "Europe/London",
-        }
-      );
-    });
-
+        });
+      },
+      {
+        scheduled: true,
+        timezone: "Europe/London",
+      }
+    );
+  });
 });
 module.exports = sendmailRouter;
